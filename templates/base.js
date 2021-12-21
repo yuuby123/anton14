833b19c199fcd7e368216cce3b1c8c0e313b0af5
yuuby123 <79774539+yuuby123@users.noreply.github.com>
2021-12-21 22:37:43 +0700

    Update base.js

--- templates/base.js
+++ templates/base.js
@@ -1,21 +1,8 @@
-var tinggi,berat,keterangan,bmi;
-function hitung() {
-	tinggi = parseFloat(document.getElementById("ipt_t").value);
-	tinggi /= 100;
-	berat = parseFloat(document.getElementById("ipt_b").value);
-	bmi = berat / (tinggi * tinggi);
+let btn = document.getElementById('btn');
 
-	if (bmi > 27) {
-		keterangan = "Gemuk, Kelebihan berat badan tingkat berat";
-	}else if ((bmi >= 25.1) & (bmi <= 27)){
-		keterangan = "Gemuk, Kelebihan berat badan tingkat ringan";
-	}else if ((bmi >= 18.5) & (bmi <= 25)){
-		keterangan = "Normal";
-	} else if ((bmi >= 17) & (bmi <= 18.4)){
-		keterangan = "Kurus, Kekurangan berat badan tingkat ringan";
-	}else {
-		keterangan = "Kurus, Kekurangan berat badan tingkat berat";
-	}
-	document.getElementById('bmi').innerHTML = "Hasil perhitungan BMI : " + bmi.toFixed(1);
-	document.getElementById('keterangan').innerHTML = keterangan;
-}
+btn.addEventListener('click', function(){
+    let weight = document.getElementById('weight-input').value;
+    let height = document.getElementById('height-input').value;
+    let finalbmi = (weight / (height * height) * 10000);
+    document.getElementById('bmi-output').value = finalbmi;
+})
